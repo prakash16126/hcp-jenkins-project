6cc074499fdcaf20547b2c9a909d902c0a59d268
Prakash Kuppannagari <146553270+prakash16126@users.noreply.github.com>
2026-08-24 22:15:29 +0530

    Change instance type and update instance names

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 resource "aws_instance" "one" {
 count = 0
 ami = "ami-0b6d9d3d33ba97d99"
-instance_type = "t2.medium"
+instance_type = "c7i.xlarge"
 key_name = "mykeypair"
 vpc_security_group_ids = ["sg-0ecaecbf03be6183e"]
 tags = {
@@ -14,6 +14,6 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins", "APPSERVER-1", "Monitoring server"]
+default = ["jenkins", "APP SERVER", "Monitoring server"]
 }
 
